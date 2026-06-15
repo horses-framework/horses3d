3d7b06002bb15f84046977e6d5cb0a6ef6e09e36
Hatem Kessasra <108345480+HatemKsr@users.noreply.github.com>
2026-06-15 11:45:08 +0200

    Revert to previous InterpolationMatrices implementation for h-mortars

--- Solver/src/libs/mesh/FaceClass.f90
+++ Solver/src/libs/mesh/FaceClass.f90
@@ -208,19 +208,20 @@
 !
 !////////////////////////////////////////////////////////////////////////
 !
-   SUBROUTINE Face_LinkWithElements( self, NelLeft, NelRight, nodeType, offset, s)
-      IMPLICIT NONE
-      class(Face)        ,     intent(INOUT) :: self        ! Current face
-      integer,                 intent(in)    :: NelLeft(2)  ! Left element face polynomial order
-      integer,                 intent(in)    :: NelRight(2) ! Right element face polynomial order
-      integer,                 intent(in)    :: nodeType    ! Either Gauss or Gauss-Lobatto
-      real(kind=RP), optional, intent(in)    :: offset(2)   ! Offset for h-mortars or sliding meshes
-      real(kind=RP), optional, intent(in)    :: s(2)        ! Scale for h-mortars or sliding meshes
+      SUBROUTINE Face_LinkWithElements( self, NelLeft, NelRight, nodeType, offset, s)
+         IMPLICIT NONE
+         class(Face)        ,     intent(INOUT) :: self        ! Current face
+         integer,                 intent(in)    :: NelLeft(2)  ! Left element face polynomial order
+         integer,                 intent(in)    :: NelRight(2) ! Right element face polynomial order
+         integer,                 intent(in)    :: nodeType    ! Either Gauss or Gauss-Lobatto
+         real(kind=RP), optional, intent(in)    :: offset(2)
+         real(kind=RP), optional, intent(in)    :: s(2)
 
 #if (!defined(NAVIERSTOKES)) && (!defined(INCNS))
          logical  :: computeGradients = .true.
 #endif
-!
+
+!     
 !     -------------------------------------------------------------
 !     First, get face elements polynomial orders (without rotation)
 !     -------------------------------------------------------------
@@ -266,49 +267,51 @@
 !     -----------------------------------------------------------------------
 !
       if (.not.present(offset)) then 
-            call Tset(self % NfLeft(1), self % Nf(1)) % construct(self % NfLeft(1), self % Nf(1))
-            call Tset(self % Nf(1), self % NfLeft(1)) % construct(self % Nf(1), self % NfLeft(1))
-            
-            call Tset(self % NfLeft(2), self % Nf(2)) % construct(self % NfLeft(2), self % Nf(2))
-            call Tset(self % Nf(2), self % NfLeft(2)) % construct(self % Nf(2), self % NfLeft(2))
+         call Tset(self % NfLeft(1), self % Nf(1)) % construct(self % NfLeft(1), self % Nf(1))
+         call Tset(self % Nf(1), self % NfLeft(1)) % construct(self % Nf(1), self % NfLeft(1))
+         
+         call Tset(self % NfLeft(2), self % Nf(2)) % construct(self % NfLeft(2), self % Nf(2))
+         call Tset(self % Nf(2), self % NfLeft(2)) % construct(self % Nf(2), self % NfLeft(2))
 
-            call Tset(self % NfRight(1), self % Nf(1)) % construct(self % NfRight(1), self % Nf(1))
-            call Tset(self % Nf(1), self % NfRight(1)) % construct(self % Nf(1), self % NfRight(1))
-            
-            call Tset(self % NfRight(2), self % Nf(2)) % construct(self % NfRight(2), self % Nf(2))
-            call Tset(self % Nf(2), self % NfRight(2)) % construct(self % Nf(2), self % NfRight(2))
-   
+         call Tset(self % NfRight(1), self % Nf(1)) % construct(self % NfRight(1), self % Nf(1))
+         call Tset(self % Nf(1), self % NfRight(1)) % construct(self % Nf(1), self % NfRight(1))
+         
+         call Tset(self % NfRight(2), self % Nf(2)) % construct(self % NfRight(2), self % Nf(2))
+         call Tset(self % Nf(2), self % NfRight(2)) % construct(self % Nf(2), self % NfRight(2))
 
+
+      end if 
+      if (present(offset) .and. (.not.present(s))) then !4:1
+         call TsetM(self % NfLeft(1), self % Nf(1), 2, 1) % construct(self % NfLeft(1), self % Nf(1), 0.5_RP, 0.5_RP, 1)  !0.5_RP, 0.5_RP
+         call TsetM(self % Nf(1), self % NfLeft(1), 2, 2) % construct(self % Nf(1), self % NfLeft(1), 0.5_RP, 0.5_RP, 2)  !0.5_RP, 0.5_RP
+
+         call TsetM(self % NfLeft(1), self % Nf(1), 1, 1) % construct(self % NfLeft(1), self % Nf(1), -0.5_RP, 0.5_RP, 1) !-0.5_RP, 0.5_RP
+         call TsetM(self % Nf(1), self % NfLeft(1), 1, 2) % construct(self % Nf(1), self % NfLeft(1), -0.5_RP, 0.5_RP, 2) !-0.5_RP, 0.5_RP
+         
+         call TsetM(self % NfLeft(2), self % Nf(2), 2, 1) % construct(self % NfLeft(2), self % Nf(2), 0.5_RP, 0.5_RP, 1)  !0.5_RP, 0.5_RP
+         call TsetM(self % Nf(2), self % NfLeft(2), 2, 2) % construct(self % Nf(2), self % NfLeft(2), 0.5_RP, 0.5_RP, 2)  !0.5_RP, 0.5_RP
+
+         call TsetM(self % NfLeft(2), self % Nf(2), 1, 1) % construct(self % NfLeft(2), self % Nf(2), -0.5_RP, 0.5_RP, 1)  !-0.5_RP, 0.5_RP
+         call TsetM(self % Nf(2), self % NfLeft(2), 1, 2) % construct(self % Nf(2), self % NfLeft(2), -0.5_RP, 0.5_RP, 2)  !-0.5_RP, 0.5_RP
+      end if 
+      if (present(offset) .and. present(s)) then !Sliding
+         if (self% Mortarpos==0) then!!!!!!0  !!!!1
+            call TsetM(self % NfLeft(1), self % Nf(1), 1, 1) % construct(self % NfLeft(1), self % Nf(1), offset(2), s(1), 1)
+            call TsetM(self % Nf(1), self % NfLeft(1), 1, 2) % construct(self % Nf(1), self % NfLeft(1), offset(2), s(1), 2)
+
+            call TsetM(self % NfLeft(1), self % Nf(1), 2, 1) % construct(self % NfLeft(1), self % Nf(1), offset(1), s(2), 1)    
+            call TsetM(self % Nf(1), self % NfLeft(1), 2, 2) % construct(self % Nf(1), self % NfLeft(1), offset(1), s(2), 2)
+         else if (self%Mortarpos==1)  then!!!!!!1  !!!!!0
+            call TsetM(self % NfLeft(1), self % Nf(1), 3, 1) % construct(self % NfLeft(1), self % Nf(1), offset(2), s(1), 1)       
+            call TsetM(self % Nf(1), self % NfLeft(1), 3, 2) % construct(self % Nf(1), self % NfLeft(1), offset(2), s(1), 2)
+
+            call TsetM(self % NfLeft(1), self % Nf(1), 4, 1) % construct(self % NfLeft(1), self % Nf(1), offset(1), s(2), 1)    
+            call TsetM(self % Nf(1), self % NfLeft(1), 4, 2) % construct(self % Nf(1), self % NfLeft(1), offset(1), s(2), 2)
          end if 
-         if (present(offset) .and. (.not.present(s))) then !4:1 mortars
-            call TsetM(self % NfLeft(1), self % Nf(1), 2, 1) % construct(self % NfLeft(1), self % Nf(1), 0.5_RP, 0.5_RP)  !0.5_RP, 0.5_RP
-            call TsetM(self % Nf(1), self % NfLeft(1), 2, 2) % construct(self % Nf(1), self % NfLeft(1), 0.5_RP, 0.5_RP)  !0.5_RP, 0.5_RP
+      end if 
+      
+
    
-            call TsetM(self % NfLeft(1), self % Nf(1), 1, 1) % construct(self % NfLeft(1), self % Nf(1), -0.5_RP, 0.5_RP) !-0.5_RP, 0.5_RP
-            call TsetM(self % Nf(1), self % NfLeft(1), 1, 2) % construct(self % Nf(1), self % NfLeft(1), -0.5_RP, 0.5_RP) !-0.5_RP, 0.5_RP
-            
-            call TsetM(self % NfLeft(2), self % Nf(2), 2, 1) % construct(self % NfLeft(2), self % Nf(2), 0.5_RP, 0.5_RP)  !0.5_RP, 0.5_RP
-            call TsetM(self % Nf(2), self % NfLeft(2), 2, 2) % construct(self % Nf(2), self % NfLeft(2), 0.5_RP, 0.5_RP)  !0.5_RP, 0.5_RP
-   
-            call TsetM(self % NfLeft(2), self % Nf(2), 1, 1) % construct(self % NfLeft(2), self % Nf(2), -0.5_RP, 0.5_RP)  !-0.5_RP, 0.5_RP
-            call TsetM(self % Nf(2), self % NfLeft(2), 1, 2) % construct(self % Nf(2), self % NfLeft(2), -0.5_RP, 0.5_RP)  !-0.5_RP, 0.5_RP
-
-         end if 
-         if (present(offset) .and. present(s)) then !Sliding mortars
-            if (self% Mortarpos==0) then
-               call TsetM(self % NfLeft(1), self % Nf(1), 1, 1) % construct(self % NfLeft(1), self % Nf(1), offset(2), s(1))
-               call TsetM(self % Nf(1), self % NfLeft(1), 1, 2) % construct(self % Nf(1), self % NfLeft(1), offset(2), s(1))
-
-               call TsetM(self % NfLeft(1), self % Nf(1), 2, 1) % construct(self % NfLeft(1), self % Nf(1), offset(1), s(2))    
-               call TsetM(self % Nf(1), self % NfLeft(1), 2, 2) % construct(self % Nf(1), self % NfLeft(1), offset(1), s(2))
-            else if (self%Mortarpos==1)  then
-               call TsetM(self % NfLeft(1), self % Nf(1), 3, 1) % construct(self % NfLeft(1), self % Nf(1), offset(2), s(1))       
-               call TsetM(self % Nf(1), self % NfLeft(1), 3, 2) % construct(self % Nf(1), self % NfLeft(1), offset(2), s(1))
-
-               call TsetM(self % NfLeft(1), self % Nf(1), 4, 1) % construct(self % NfLeft(1), self % Nf(1), offset(1), s(2))    
-               call TsetM(self % Nf(1), self % NfLeft(1), 4, 2) % construct(self % Nf(1), self % NfLeft(1), offset(1), s(2))
-            end if 
-         end if
 !
 !     -----------------------  0- no projection
 !     Set the projection type: 1- x needs projection
